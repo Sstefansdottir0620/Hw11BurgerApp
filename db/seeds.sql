ebae36aab5255e48a8d67fa1c10078d6869c854c
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-03-02 22:47:13 -0600

    added routes and connected database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burger (name, devoured) VALUES ('Steak Bernaise Burger', false);
+INSERT INTO burger (name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burger (name, devoured) VALUES ('Bacon Supreme Burger', false);
+INSERT INTO burger (name, devoured) VALUES ('Blue Cheese Burger', false);
+INSERT INTO burger (name, devoured) VALUES ('Chick Pee Burger', false);
