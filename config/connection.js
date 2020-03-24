c9d0c4f742bd63d818f86246d258ff474990aa29
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-03-23 19:23:35 -0500

    update to server.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+  host: "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "smp81ml55r5ksw77",
   password: "h716i8103oz1htb2",
